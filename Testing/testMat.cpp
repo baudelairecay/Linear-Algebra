f334f9185b9662c2d82d4f8d45dd9aadb90a7d26
Cayden Hutt <caydenhutt208@gmail.com>
2026-06-27 18:45:11 -0400

    removed testMat.cpp

--- Testing/testMat.cpp
+++
@@ -1,9 +0,0 @@
-#include "../matrix.hpp"
-
-
-int main(void){
-    vector<vector<float>> els = {};
-    // Test empty matrix
-    Matrix mat = Matrix(els); // should throw a runtime error
-    return 0;
-}
