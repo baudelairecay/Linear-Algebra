e86b7528530802425caa3a3071f2962a3f2cdc39
baudelairecay <156119001+baudelairecay@users.noreply.github.com>
2026-06-27 18:26:33 -0400

    Delete Testing/Test.cpp

--- Testing/Test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main(void){
-    vector<vector<int>> A = {{1,2}, {2,1}};
-    vector<vector<int>> B = {{1,2}, {2,1}};
-    vector<vector<int>> C;
-}
