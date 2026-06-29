640c1acbae09f4aaafd6a8ad5d632f421eb81618
Cayden Hutt <caydenhutt208@gmail.com>
2026-06-28 21:35:27 -0400

    finished testTranspose.cpp

--- Testing/testTranspose.cpp
+++ Testing/testTranspose.cpp
@@ -1,35 +1,50 @@
 #include "../matrix.hpp"
 using namespace std;
 
-// I know this isn't the most efficient way to test, I'll write this later
 void visualTestTranpose(Matrix mat){
-    cout << "matrix:" << endl;
     mat.print();
     Matrix transposed = transpose(mat);
     cout << "transpose of matrix:" << endl;
     transposed.print();
 }
 
-void testTranspsoe(Matrix mat, vector<vector<int>> expected){
-    
+void testTranspsoe(Matrix mat, vector<vector<float>> expected){
+    Matrix transposed = transpose(mat);
+    if(transposed.getElements() == expected){
+        cout << "all is well" << endl;
+    }else{
+        throw runtime_error("Output does not equal output");
+    }
 }
 int main(void){
-    vector<vector<float>> vec1 = {{1,1}, {1,1}};
-    vector<vector<float>> vec2 = {{1,1,1}, {1,1,1}};
-    vector<vector<float>> vec3 = {{1,1}, {1,1}, {1,1}};
-    Matrix mat1 = Matrix(vec1);
-    Matrix mat2 = Matrix(vec2);
-    Matrix mat3 = Matrix(vec3);
+    vector<vector<float>> vec = {{1,1}, {1,1}};
+    vector<vector<float>> expected = {{1,1}, {1,1}};
+    Matrix mat = Matrix(vec);
 
     cout << "square matrix test:" << endl; 
-    visualTestTranpose(mat1);
+    visualTestTranpose(mat);
+    testTranspsoe(mat, expected);
 
     cout << "more columns than rows test:" << endl; 
-    visualTestTranpose(mat2);
+    vec = {{1,2,3}, {5,4,7}};
+    expected = {{1,5}, {2,4}, {3,7}};
+    mat = Matrix(vec);
+    visualTestTranpose(mat);
+    testTranspsoe(mat, expected);
 
     cout << "more rows than columns test:" << endl; 
-    visualTestTranpose(mat3);
+    vec = {{0,7}, {9,5}, {3,4}};
+    expected = {{0,9,3}, {7,5,4}};
+    mat = Matrix(vec);
+    visualTestTranpose(mat);
+    testTranspsoe(mat, expected);
 
+    cout << "empty matrix test:" << endl;
+    vec = {};
+    expected = {};
+    mat = Matrix(vec);
+    visualTestTranpose(mat);
+    testTranspsoe(mat,expected);
     return 0;
 
 }
